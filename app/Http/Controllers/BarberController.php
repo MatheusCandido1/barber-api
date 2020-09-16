f8acd871b3117b5221dffa1ff796dda6a2b6c544
Matheus Cândido <matheus9bh@hotmail.com>
2020-09-16 19:00:58 -0300

    add-random-barber

--- app/Http/Controllers/BarberController.php
+++ app/Http/Controllers/BarberController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Barber;
+use App\Models\BarberImage;
+use App\Models\BarberService;
+use App\Models\BarberTestimonials;
+use App\Models\BarberAvailability;
 
 class BarberController extends Controller
 {
-    //
+    private $currentUser;
+
+    public function __construct() {
+        $this->middleware('auth:api');
+        $this->currentUser = auth()->user();
+    }
+
+    public function random() {
+         
+    }
 }
