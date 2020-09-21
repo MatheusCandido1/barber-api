64906a1af0eda1f8e6751b22d6a619d72d16ceec
Matheus Cândido <matheus9bh@hotmail.com>
2020-09-21 13:11:20 -0300

    add-user-profile-info

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    private $currentUser;
+
+    public function __construct() {
+        $this->middleware('auth:api');
+        $this->currentUser = auth()->user();
+    }
+
+    public function details() {
+        $user = $this->currentUser;
+        $user['avatar'] = url('media/avatars/'.$user['avatar']);
+
+        return response()->json([
+            'data' => $user,
+        ], 200);
+    }
 }
